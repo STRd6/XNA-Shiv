30642b9d386cd8d1c17dae88eaadb2d27e71c47a
Daniel X. Moore <yahivin@gmail.com>
2011-08-19 14:05:10 -0400

    Modified in browser at pixie.strd6.com

--- XNA-Shiv.js
+++ XNA-Shiv.js
@@ -2,19 +2,16 @@
 ;
 var Canvas;
 Canvas = function() {
-  return {
-    __proto__: Canvas.prototype
-  };
-};
-Canvas.prototype = {
-  withTransform: function(matrix, block) {
-    this.begin(matrix.a, matrix.b, matrix.c, matrix.d, matrix.tx, matrix.ty);
-    try {
-      block(this);
-    } finally {
-      this.end();
+  return Object.extend({}, __XNA__Canvas, {
+    withTransform: function(matrix, block) {
+      this.begin(matrix.a, matrix.b, matrix.c, matrix.d, matrix.tx, matrix.ty);
+      try {
+        block(this);
+      } finally {
+        this.end();
+      }
+      return this;
     }
-    return this;
-  }
+  });
 };;
 ;
